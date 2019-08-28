3b700140782b3ff164d59b5bae4237e92d67f8c7
Michael Cai <michael.cai@natixis.com>
2019-08-28 13:51:31 -0400

    Made changes to Junit so that it uses HtmlUnit

--- ch02/tacos/src/test/java/tacos/DesignTacoControllerTest.java
+++ ch02/tacos/src/test/java/tacos/DesignTacoControllerTest.java
@@ -1,23 +1,17 @@
 // tag::testShowDesignForm[]
 package tacos;
-import static org.mockito.Mockito.verify;
-import static 
-    org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static 
-    org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
 import tacos.Ingredient.Type;
 import tacos.web.DesignTacoController;
 
